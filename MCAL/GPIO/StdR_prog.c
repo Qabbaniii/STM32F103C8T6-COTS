a2d6a025ace6dbd74c41f6dbd02ca76ecc68c116
Abdullah Qabbani <mhamad50513@gmail.com>
2024-02-28 03:36:26 +0200

    Creating GPIO Driver

---
+++ MCAL/GPIO/StdR_prog.c
@@ -0,0 +1,60 @@
+
+/********************************************************************************
+ *	FILE DESCRIPTION
+ *********************************************************************************
+ * Filename              :   StdR_Program.c
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   00/00/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :
+ * Notes                 :
+ *
+ *********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	    Version   Author             Description
+ *  00/00/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+#ifdef
+#ifndef
+
+/**********************************************************************************
+ * 	INCLUDES
+ **********************************************************************************/
+#include "stdint.h"
+#include "../../SERVICES/BIT_MATH.h"
+#include "../../SERVICES/STD_TYPES.h"
+#include "StdR_Config.h"
+#include "StdR_Interface.h"
+#include "StdR_Register.h"
+
+
+/*********************************************************************************
+ *	LOCAL FUNCTIONS
+ *********************************************************************************/
+
+/*********************************************************************************
+ *	GLOBAL FUNCTIONS
+ *********************************************************************************/
+
+
+/*
+ * Prototype        : StdR_ReturnType FunctionName(AnyType parameterName)
+ * Description      : Describe this service
+ * Parameters (in)  : parameterName   Parameter Describtion                     
+ * Parameters (out) : None    
+ * return           : StdR_ReturnType
+ */
+
+
+Std_ReturnType FunctionName(AnyType parameterName)
+{
+	
+	
+}
+
+/**************************************************************************************
+ *  END OF FILE: StdR_Program.c
+ **************************************************************************************/
