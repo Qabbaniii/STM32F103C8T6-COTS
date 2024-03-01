9fb5a82639aa06628f1a3b3a66ff81d7128f0484
Abdullah Qabbani <mhamad50513@gmail.com>
2024-03-01 03:51:24 +0200

    Adding GPIO Driver

---
+++ MCAL/GPIO/GPIO_interface.h
@@ -0,0 +1,118 @@
+
+/**********************************************************************************
+ *	FILE DESCRIPTION
+ **********************************************************************************
+ * Filename              :   GPIO_interface.h
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   29/02/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :
+ * Notes                 :	
+ *
+ **********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	   		 Version   Author             Description
+ *  29/02/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+
+
+/***********************************************************************************
+ * 	INCLUDES
+ **********************************************************************************/
+#include "../../LIB/STD_TYPES.h"
+/***********************************************************************************
+ *	CONSTANT MACROS & ENUMS
+ ***********************************************************************************/
+
+typedef enum
+{
+    PORTA = 0,
+    PORTB,
+    PORTC,
+	PORTD,
+	PORTE,
+	PORTF,
+	PORTG
+} Port_t;
+typedef enum
+{
+    PIN0 = 0,
+    PIN1,
+    PIN2,
+    PIN3,
+    PIN4,
+    PIN5,
+    PIN6,
+    PIN7,
+    PIN8,
+    PIN9,
+    PIN10,
+    PIN11,
+    PIN12,
+    PIN13,
+    PIN14,
+    PIN15
+} Pin_t;
+typedef enum
+{
+    ANALOG = 0,
+    INPUT_FLOATING,
+	INPUT_PULL,
+} InputMode_t;
+
+typedef enum
+{
+    OUT_PUSH_PULL = 0,
+	OUT_OPEN_DRAIN,
+    AF_PUSH_PULL,
+    AF_OPEN_DRAIN
+} OutputMode_t;
+
+typedef enum
+{
+    INPUT = 0,
+    OUTPUT_10MHZ,
+    OUTPUT_2MHZ,
+    OUTPUT_50MHZ
+} Mode_t;
+
+typedef enum
+{
+    PIN_LOW,
+    PIN_HIGH
+} PinVal_t;
+
+#define TWO_PIN_ACCESS                  2u
+#define FOUR_PIN_ACCESS                 4u
+
+#define TWO_BIT_MASK                    0b11
+#define ONE_BIT_MASK                    0b1
+
+#define OK 								1
+#define NOK 							0
+
+typedef struct
+{
+    Port_t 		 PortName;
+    Pin_t 	  	 PinNum;
+	Mode_t 		 Mode;
+    InputMode_t  InputMode;
+    OutputMode_t OutputMode;
+
+} PinConfig_t;
+
+/***********************************************************************************
+ *	FUNCTION PROTOTYPES
+ **********************************************************************************/
+
+uint8_t GPIO_u8PinInit(const PinConfig_t *PinConfig);
+uint8_t GPIO_u8SetPinValue(Port_t PortName, Pin_t PinNum, PinVal_t PinVal);
+uint8_t GPIO_u8TogglePinValue(Port_t PortName, Pin_t PinNum);
+uint8_t GPIO_u8ReadPinValue(Port_t PortName, Pin_t PinNum, PinVal_t *PinVal);
+
+/**********************************************************************************
+ *	END OF FILE: GPIO_interface.h
+ **********************************************************************************/
---
+++ MCAL/GPIO/GPIO_prog.c
@@ -0,0 +1,157 @@
+/********************************************************************************
+ *	FILE DESCRIPTION
+ *********************************************************************************
+ * Filename              :   GPIO_Program.c
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   29/02/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :
+ * Notes                 :
+ *
+ *********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	    	 Version   Author             Description
+ *  29/02/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+
+/**********************************************************************************
+ * 	INCLUDES
+ **********************************************************************************/
+#include "stdint.h"
+#include "../../LIB/BIT_MATH.h"
+#include "../../LIB/STD_TYPES.h"
+#include "../../LIB/STM32F103.h"
+#include "GPIO_Interface.h"
+
+
+/* static array of 8 pointers (the GPIO Ports) to avoid using "switch case" */
+
+static GPIO_RegDef_t *GPIO_PORT[7] = {GPIOA, GPIOB, GPIOC, GPIOD, GPIOE, GPIOF, GPIOG};
+
+/*********************************************************************************
+ *	GLOBAL FUNCTIONS
+ *********************************************************************************/
+
+/*
+ * Prototype        : uint8_t GPIO_u8PinInit(const PinConfig_t * PinConfig);
+ * Description      : initializes the GPIO pin according to the input paramters
+ * Parameters (in)  : PinConfig  :  A struct of paramters to config the pin like :
+									PortName,pinNum,Mode,InputMode,OutputMode
+ * Parameters (out) : None                          
+ * return           : ErrorStatus   OK(0) means no error,NOK(1) means Undefined PORT Argument
+ */                                                 
+                                                    
+uint8 GPIO_u8PinInit(const PinConfig_t * PinConfig) 
+{
+	uint8_t Local_u8ErrorStatus = OK;
+		if((PinConfig->PortName <= PORTG) && (PinConfig->PinNum <= PIN15) )
+		{
+			uint8_t LOC_u8RegNum = (PinConfig->PinNum) / 8;
+			uint8_t LOC_u8BitNum = ((PinConfig->PinNum) % 8) * 4;
+
+			/*Selecting Mode */
+			GPIO_PORT[PinConfig->PortName]->CR[LOC_u8RegNum] &= ~(TWO_BIT_MASK << LOC_u8BitNum);
+			GPIO_PORT[PinConfig->PortName]->CR[LOC_u8RegNum] |= ((PinConfig->Mode) << LOC_u8BitNum);
+
+			/*Selecting Mode Type*/
+			if (PinConfig->Mode > INPUT)
+			{
+				GPIO_PORT[PinConfig->PortName]->CR[LOC_u8RegNum] &= ~(TWO_BIT_MASK << (LOC_u8BitNum + 2));
+				GPIO_PORT[PinConfig->PortName]->CR[LOC_u8RegNum] |= ((PinConfig->OutputMode) << (LOC_u8BitNum + 2));
+			}
+			else
+			{
+				GPIO_PORT[PinConfig->PortName]->CR[LOC_u8RegNum] &= ~(TWO_BIT_MASK << (LOC_u8BitNum + 2));
+				GPIO_PORT[PinConfig->PortName]->CR[LOC_u8RegNum] |= ((PinConfig->InputMode) << (LOC_u8BitNum + 2));
+			}
+		}
+		else
+		{
+			Local_u8ErrorStatus = NOK;
+		}
+		return Local_u8ErrorStatus;
+}
+
+/*
+ * Prototype        : uint8 GPIO_u8SetPinValue(Port_t PortName ,Pin_t pinNum,PinVal_t PinVal)
+ * Description      : set a certain value on an output pin
+ * Parameters (in)  : PortName [PORTA - PORTH] 
+					  pinNum   [PIN0 - PIN15]
+					  PinVal   PIN_LOW,PIN_HIGH            
+ * Parameters (out) : None    
+ * return           : ErrorStatus    OK(0) means no error,NOK(1) means Undefined PORT Argument
+ */
+ 
+uint8 GPIO_u8SetPinValue(Port_t portName ,Pin_t pinNum,PinVal_t PinVal)
+{
+	uint8_t Local_u8ErrorStatus = OK;
+		if ((portName <= PORTC) && (pinNum <= PIN15))
+		{
+			if (PinVal == PIN_HIGH)
+			{
+				GPIO_PORT[portName]->ODR |= (ONE_BIT_MASK << pinNum);
+			}
+			else if (PinVal == PIN_LOW)
+			{
+				GPIO_PORT[portName]->ODR &= ~(ONE_BIT_MASK << pinNum);
+			}
+		}
+		else
+		{
+			Local_u8ErrorStatus = NOK;
+		}
+		return Local_u8ErrorStatus;
+}
+
+/*
+ * Prototype        : uint8 GPIO_u8TogglePinValue(Port_t PortName ,Pin_t pinNum);
+ * Description      : toggles the value of a certain pin
+ * Parameters (in)  : PortName [PORTA - PORTH] 
+					  pinNum   [PIN0 - PIN15]
+ * Parameters (out) : None    
+ * return           : ErrorStatus    OK(0) means no error,NOK(1) means Undefined PORT Argument
+ */
+
+uint8 GPIO_u8TogglePinValue(Port_t portName ,Pin_t pinNum)
+{
+	uint8_t Local_u8ErrorStatus = OK;
+	if ((portName <= PORTC) && (pinNum <= PIN15))
+	{
+		GPIO_PORT[portName]->ODR ^= (ONE_BIT_MASK << pinNum);
+	}
+	else
+	{
+		Local_u8ErrorStatus = NOK;
+	}
+	return Local_u8ErrorStatus;
+}
+
+/*
+ * Prototype        : uint8 GPIO_vReadPinValue(PORT_t Port ,Pin_t pinNum,PinVal_t* PinVal);
+ * Description      : reads the value of a certain pin
+ * Parameters (in)  : PortName [PORTA - PORTH]
+					  pinNum   [PIN0 - PIN15]
+ * Parameters (out) : PinVal   0,1   
+ * return           : ErrorStatus
+ */
+ uint8 GPIO_u8ReadPinValue(Port_t portName ,Pin_t pinNum,PinVal_t* PinVal)
+ {
+	 uint8_t Local_u8ErrorStatus = OK;
+	 	if ((portName <= PORTC) && (pinNum <= PIN15))
+	 	{
+	 		*PinVal = (GPIO_PORT[portName]->IDR >> pinNum) & ONE_BIT_MASK;
+	 	}
+	 	else
+	 	{
+	 		Local_u8ErrorStatus = NOK;
+	 	}
+	 	return Local_u8ErrorStatus;
+ }
+
+
+/**************************************************************************************
+ *  END OF FILE: GPIO_Program.c
+ **************************************************************************************/
