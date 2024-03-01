6a8093c3abea6d64267a89d84ef6fb22954a1c50
Abdullah Qabbani <mhamad50513@gmail.com>
2024-03-01 03:52:01 +0200

    Adding LED Driver

---
+++ HAL/LED/LED_interface.h
@@ -0,0 +1,33 @@
+﻿
+/**********************************************************************************
+ *	FILE DESCRIPTION
+ **********************************************************************************
+ * Filename              :   LED_interface.h
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   01/03/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :
+ * Notes                 :	
+ *
+ **********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	    	Version   Author             Description
+ *  01/03/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+
+
+#ifndef HAL_LED_LED_INTERFACE_H_
+#define HAL_LED_LED_INTERFACE_H_
+
+#include"../../MCAl/GPIO/GPIO_interface.h"
+
+    /*  Function Prototype  */
+void LED_vInit   (uint8 copy_u8PortName,uint32 copy_u32PinNumber);
+void LED_vOn   (uint8 copy_u8PortName,uint32 copy_u32PinNumber);
+void LED_vOff  (uint8 copy_u8PortName,uint32 copy_u32PinNumber);
+void LED_vToggle (uint8 copy_u8PortName,uint32 copy_u32PinNumber);
+void LED_vGetStatus (uint8 copy_u8PortName,uint32 copy_u32PinNumber);
+#endif /* HAL_LED_LED_INTERFACE_H_ */
---
+++ HAL/LED/LED_prog.c
@@ -0,0 +1,57 @@
+﻿/**********************************************************************************
+ *	FILE DESCRIPTION
+ **********************************************************************************
+ * Filename              :   LED_proj.h
+ * Author                :   Abdullah Qabbani
+ * Origin Date           :   01/03/2024
+ * Version               :   1.0.0
+ * Compiler              :   GCC ARM Embedded Toolchain
+ * Target                :
+ * Notes                 :	
+ *
+ **********************************************************************************/
+/************************ SOURCE REVISION LOG *************************************
+ *
+ *  Date 	    	 Version   Author             Description
+ *  01/03/2024       1.0.0     Abdullah Qabbani   Initial Release.
+ *
+ **********************************************************************************/
+
+
+#include <stdio.h>
+#include"LED_interface.h"
+
+
+
+ void LED_vInit  (uint8 copy_u8PortName,uint32 copy_u32PinNumber)
+{
+	  PinConfig_t PinConfig = {
+			copy_u8PortName,
+			copy_u32PinNumber,
+			OUTPUT_10MHZ,
+			OUT_PUSH_PULL};
+	GPIO_u8PinInit(& PinConfig);
+}
+
+void LED_vOn (uint8 copy_u8PortName,uint32 copy_u32PinNumber){
+
+	GPIO_u8SetPinValue(copy_u8PortName,copy_u32PinNumber,PIN_HIGH);
+}
+
+void LED_vOff (uint8 copy_u8PortName,uint32 copy_u32PinNumber){
+
+	GPIO_u8SetPinValue(copy_u8PortName,copy_u32PinNumber,PIN_LOW);
+}
+
+void LED_vToggle (uint8 copy_u8PortName,uint32 copy_u32PinNumber)
+{
+	
+	GPIO_u8TogglePinValue(copy_u8PortName,copy_u32PinNumber);
+}
+
+
+void LED_u8GetStatus (uint8 copy_u8PortName,uint32 copy_32PinNumber,PinVal_t *PinVal)
+{
+
+	GPIO_u8ReadPinValue(copy_u8PortName,copy_32PinNumber,PinVal);
+}
